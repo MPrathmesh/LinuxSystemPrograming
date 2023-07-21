1193ebff3c34fecb5e41c2be721557f08f6875cc
MPrathmesh <108971620+MPrathmesh@users.noreply.github.com>
2023-07-21 18:13:02 +0530

    Add files via upload

--- A5Q4.c
+++ A5Q4.c
@@ -2,57 +2,84 @@
 //from the directory into one file name as AllCombine.txt.
 #include<stdio.h>
 #include<stdlib.h>
-#include<string.h>
-#include<unistd.h>
-#include<fcntl.h>
 #include<dirent.h>
+#include<fcntl.h>
+#include<unistd.h>
 #include<sys/stat.h>
 
-#pragma pack(1)
-struct FileInfo
+void combineFiles(char *Dirname)
 {
-    char FileName[20];
-    int FileSize;
-};
-
-int main(int argc, char *argv[])
-{
-    char DirName[20];
-    DIR *dp = NULL;
+    DIR *dp = opendir(Dirname);
+    int fd1 = 0;
     struct dirent *entry = NULL;
-    char name[30];
     struct stat sobj;
-    char namecopy[30] = {'\0'};
-    int iMax = 0;
-    struct FileInfo fobj;
-    int fd = 0;
+    char Buffer[1024];
+    int fd2 = 0;
+    char filePath[50];
 
-    printf("Enter name of directory : \n");
-    scanf("%s",DirName);
 
-    dp = opendir(DirName);
     if(dp == NULL)
     {
         printf("Unable to open directory\n");
-        return -1;
+        return;
     }
 
-    fd = creat("FileCombine.txt",0777);
+    fd1 = open("AllCombine.txt", O_WRONLY | O_CREAT, 0777);
+    if(fd1 == -1)
+    {
+        printf("Unable to open file\n");
+        closedir(dp);
+        return;
+    }
+
 
     while((entry = readdir(dp)) != NULL)
     {
-        sprintf(name,"%s/%s",DirName,entry->d_name);
-        stat(name,&sobj);
+        sprintf(filePath,"%s/%s",Dirname, entry->d_name);
+
+        if(stat(filePath, &sobj) == -1)
+        {
+            printf("Unable to accessing the file Information\n");
+            continue; //Skip this file and move to next file
+        }
+
         if(S_ISREG(sobj.st_mode))
         {
-            fobj.FileSize = sobj.st_size;
-            strcpy(fobj.FileName,entry->d_name);
-            write(fd,&fobj,sizeof(fobj));
-            printf("%s\n",entry->d_name);
+            fd2 = open(filePath, O_RDONLY);
+            if(fd2 == -1)
+            {
+                printf("Unable to open this file\n");
+                continue; //skip this file and move to next file
+            }
+
+            int ret = 0;
+            while((ret = read(fd2, Buffer, sizeof(Buffer))) > 0)
+            {
+                if(write(fd1, Buffer, ret) == -1)
+                {
+                    printf("Unable to write to combine file\n");
+                    break;
+                }
+            }
+            close(fd2);
         }
     }
 
+    close(fd1);
     closedir(dp);
     
+    printf("Contents of all regular files combined into 'AllCombine.txt' successfully.\n");
+}
+
+int main(int argc, char *argv[])
+{
+    if(argc != 2)
+    {
+        printf("Usage: %s <directory_name>\n",argv[0]);
+        return -1;
+    }
+
+    combineFiles(argv[1]);
+
     return 0;
 }
