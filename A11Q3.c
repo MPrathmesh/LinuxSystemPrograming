dea2e1205909ea44feb13acaa28dfe771fd060c4
MPrathmesh <108971620+MPrathmesh@users.noreply.github.com>
2023-08-09 12:04:18 +0530

    Add files via upload

--- A11Q3.c
+++ A11Q3.c
@@ -1,1 +1,43 @@
 //Write a program which is used to predict the problem due to lack of synchronization.
+#include<stdio.h>
+#include<pthread.h>
+
+int counter = 0;
+
+void * ThreadProc(void *ptr)
+{
+    int i = 0;
+    counter++;
+    printf("Thread excuted with counter : %d\n", counter);
+    for(i = 0; i < 0xFFFFFFFF; i++);
+    printf("Thread completed with counter : %d\n", counter);
+    pthread_exit(NULL);
+}
+
+int main()
+{
+    pthread_t TID1, TID2;
+    int ret = 0;
+
+    ret = pthread_create(&TID1, NULL, ThreadProc, NULL);
+    if(ret != 0)
+    {
+        printf("Unbale to create to thread.\n");
+        return -1;
+    }
+
+    ret = pthread_create(&TID2, NULL, ThreadProc, NULL);
+    if(ret != 0)
+    {
+        printf("Unbale to create to thread.\n");
+        return -1;
+    }
+
+
+    pthread_join(TID1, NULL);
+    pthread_join(TID2, NULL);
+
+    printf("End of main thread.\n");
+
+    return 0;
+}
--- A11Q4.c
+++ A11Q4.c
@@ -1,2 +1,51 @@
 //Write a program which creates two thread, and by using appropriate
 //synchronization technique avoid the schedulimg problem.
+#include<stdio.h>
+#include<pthread.h>
+
+int Counter = 0;
+
+pthread_mutex_t lock;
+
+void *ThreadProc(void *)
+{
+    pthread_mutex_lock(&lock);
+    int i = 0;
+    Counter++;
+    printf("Thread excuted with counter : %d\n", Counter);
+    for(i = 0; i < 0xFFFFFFFF; i++);
+    printf("Thread completed with counter : %d\n", Counter);
+    pthread_mutex_unlock(&lock);
+    pthread_exit(NULL);
+}
+
+int main()
+{
+    pthread_t TID1, TID2;
+    int ret = 0;
+
+    pthread_mutex_init(&lock, NULL);
+
+    ret = pthread_create(&TID1, NULL, ThreadProc, NULL);
+    if(ret != 0)
+    {
+        printf("Unable to create to thread.\n");
+        return -1;
+    }
+
+    ret = pthread_create(&TID2, NULL, ThreadProc, NULL);
+    if(ret != 0)
+    {
+        printf("Unable to create to thread.\n");
+        return -1;
+    }
+
+    pthread_join(TID1, NULL);
+    pthread_join(TID2, NULL);
+
+    pthread_mutex_destroy(&lock);
+
+    printf("End of main thread.\n");
+
+    return 0;
+}
